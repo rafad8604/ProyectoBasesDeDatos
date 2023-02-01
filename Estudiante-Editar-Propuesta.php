e1fb853e236c25adaf8d07542cb3a5a2985bd9c4
rafad8604 <110276323+rafad8604@users.noreply.github.com>
2023-01-31 20:31:13 -0500

    Update Estudiante-Editar-Propuesta.php

--- Estudiante-Editar-Propuesta.php
+++ Estudiante-Editar-Propuesta.php
@@ -32,7 +32,7 @@ $row = pg_fetch_object($query);
 
     <script src="https://kit.fontawesome.com/ccd4ed56f8.js" crossorigin="anonymous"></script>
 
-    <script src="../../main.js"></script>
+    <script src="main.js"></script>
 
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
