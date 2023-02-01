16f7a9b397a5fdedc54cf97fcbc27397f2e13b3c
rafad8604 <110276323+rafad8604@users.noreply.github.com>
2023-01-31 20:05:50 -0500

    Update Director-Asignar-Propuestas.php

--- Director-Asignar-Propuestas.php
+++ Director-Asignar-Propuestas.php
@@ -20,7 +20,7 @@ $cod_proye=$_GET['id'];
 
     <!-- Conexion con la hoja de estilos -->
 
-    <link rel="stylesheet" href="../../public/css/inicio.css">
+    <link rel="stylesheet" href="css/inicio.css">
 
     <!-- Conexion con Sweet Alert -->
 
