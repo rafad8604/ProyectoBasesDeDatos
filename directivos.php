af503fc89df002ff7efcd4333b9a90dbf1c7882e
rafad8604 <110276323+rafad8604@users.noreply.github.com>
2023-01-31 20:35:31 -0500

    Update directivos.php

--- directivos.php
+++ directivos.php
@@ -9,7 +9,7 @@
 
     <!-- Conexion con la hoja de estilos -->
 
-    <link rel="stylesheet" href="../../public/css/inicio.css">
+    <link rel="stylesheet" href="css/inicio.css">
 
     <!-- Conexion con Sweet Alert -->
 
@@ -37,7 +37,7 @@
     <aside>
 
     <div class="aside-logo">
-                <img src="../../public/images/logounillanos.png" alt="">
+                <img src="images/logounillanos.png" alt="">
         </div>
 
         <div class="line"></div>
@@ -137,7 +137,7 @@
     <button onclick="toggleDropdown()">
 
         <span id="chevron">
-        <img style="opacity:0.5" src="../../public/images/profile.png" alt="" srcset="">
+        <img style="opacity:0.5" src="images/profile.png" alt="" srcset="">
         </span>
 
     </button>
