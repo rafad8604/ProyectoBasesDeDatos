af689b97d0f5375bdcb906432def8ba7b0e80e6c
rafad8604 <110276323+rafad8604@users.noreply.github.com>
2023-01-31 20:12:00 -0500

    Update estudiantes.php

--- estudiantes.php
+++ estudiantes.php
@@ -11,7 +11,7 @@
 
     <!-- Conexion con la hoja de estilos -->
 
-    <link rel="stylesheet" href="../../public/css/inicio.css">
+    <link rel="stylesheet" href="css/inicio.css">
 
     <!-- Conexion con Sweet Alert -->
 
