aabe6143821099fc020f17d738137ba3ea3c26db
rafad8604 <110276323+rafad8604@users.noreply.github.com>
2023-01-31 20:53:24 -0500

    Update visualizarEstudiante.php

--- visualizarEstudiante.php
+++ visualizarEstudiante.php
@@ -15,7 +15,7 @@ $query = pg_query($sql);
 <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <link rel="stylesheet" href="../../public/css/inicio.css">
+        <link rel="stylesheet" href="css/inicio.css">
       
         
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
