439c5f8d7c3fc44f0501549fe8da225f63f2a13e
kyubichan <110320722+kyubichan@users.noreply.github.com>
2023-02-03 06:05:07 -0500

    Update editarEst.php

--- editarEst.php
+++ editarEst.php
@@ -1,5 +1,6 @@
 <?php
 
+
 include("connection.php");
 session_start();
 
@@ -17,34 +18,43 @@ $rowd = pg_fetch_object($query);
 <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <link rel="stylesheet" href="css/inicio.css">
- 
+        <link rel="stylesheet" href="../../public/css/inicio.css">
+        
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
 </head>
 <body>
 
-<dialog open id="modal-editar" class="modal-estilo"> 
+        <dialog open id="modal-editar" class="modal-estilo"> 
 
-        <div class="contenedor-modal-editar">
+        <div class="numberCircle">
+            <img src="../../public/images/edit.png" alt="" srcset="">
+        </div>
 
-        <form action="updateEst.php" method="POST">
+        <div class="contenido-modal">
+
+        <div class="title-modal">
+          <a href="estudiantes.php"> <div id="btn-cerrar-modal"><i class="fa-solid fa-xmark"></i></div></a> 
+        </div>
+
+        <form class="formulario "action="updateEst.php" method="POST">
 
         <input value="<?php echo $codigo ?>" type="hidden" name="codigo">
 
-        <input type="text" name="dni" placeholder="Dni" value="<?php echo $rowd->dni ?>">
-        <input type="text" name="nombres" placeholder="Nombres" value="<?php echo $rowd->nomb_est ?>">
-        <input type="text" name="apellidos" placeholder="Apellidos" value="<?php echo $rowd->ape_est ?>">
+        <input class="enjoy-css" type="text" name="dni" placeholder="Dni" value="<?php echo $rowd->dni ?>">
+        <input class="enjoy-css" type="text" name="nombres" placeholder="Nombres" value="<?php echo $rowd->nomb_est ?>">
+        <input class="enjoy-css" type="text" name="apellidos" placeholder="Apellidos" value="<?php echo $rowd->ape_est ?>">
 
-        <input type="submit" value="Actualizar">
-        <a href="estudiantes.php">CERRAR</a>
+        <input class="sign-in-btn" type="submit" value="Editar">
+
 
         </form>
+
+        </div>
 
 </div>
 
 </dialog>
-
 
 </body>
 </html>
