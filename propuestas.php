bc178dd19f0b0c38d5772e7a29a7954c5d67f0d8
kyubichan <110320722+kyubichan@users.noreply.github.com>
2023-02-03 06:50:41 -0500

    Update propuestas.php

--- propuestas.php
+++ propuestas.php
@@ -10,7 +10,7 @@
 
     <!-- Conexion con la hoja de estilos -->
 
-    <link rel="stylesheet" href="../../public/css/inicio.css">
+     <link rel="stylesheet" href="css/inicio.css">
 
     <!-- Conexion con Sweet Alert -->
 
