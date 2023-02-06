3e2958b8104e34c74d93824939c06d13a106ae4c
kyubichan <110320722+kyubichan@users.noreply.github.com>
2023-02-06 04:32:30 -0500

    Update borrarEst.php

--- borrarEst.php
+++ borrarEst.php
@@ -11,7 +11,7 @@ $query = pg_query($sql);
 $consulta = pg_fetch_result($query, 0, 0);
 
 if($consulta >= 1){
-  include_once  "estudiantes.php";
+  include_once  "../views/estudiantes.php";
           echo '<script> Swal.fire({
               icon: "error",
               title: "Error",
@@ -23,7 +23,7 @@ if($consulta >= 1){
    
   if($query){
   
-      include_once  "estudiantes.php";
+      include_once  "../views/estudiantes.php";
       echo '<script> Swal.fire({
         icon: "success",
         title: "Buen trabajo!",
@@ -31,8 +31,7 @@ if($consulta >= 1){
       });</script>';
       
             
-         
-        echo " )}});</script>";
+
   
           }else{
             include_once  "../views/estudiantes.php";
